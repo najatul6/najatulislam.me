16eeb1aa2be20e5d249860144ce0af98ec1c17c0
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-11-30 18:42:49 +0600

    update hover shadow effect in SocialButton component for improved interaction feedback

--- src/Components/Shared/SocialButton/SocialButton.jsx
+++ src/Components/Shared/SocialButton/SocialButton.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const SocialButton = ({icon,link,text}) => {
   return (
     <Link to={link} target='_blank' className='flex flex-col justify-center items-center group relative'>
-    <button className="shadow-up duration-300 ease-in-out rounded-full text-2xl font-bold p-3 hover:shadow-in hover:text-blue-light active:shadow-in active:text-blue-light">
+    <button className="shadow-in duration-300 ease-in-out rounded-full text-2xl font-bold p-3 hover:shadow-up hover:text-blue-light active:shadow-in active:text-blue-light">
         {icon}
     </button>
     <p className='justify-center items-center font-Cormorant text-blue-light font-bold capitalize absolute left-0 right-0 bottom-12 hidden group-hover:flex'>{text}</p>
