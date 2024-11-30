a38d8840ad6ac15e7fe6f4dbd5377b1c123532b6
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-01 00:55:28 +0600

    refactor Button component: improve code formatting and consistency

--- src/Components/Shared/Button/Button.jsx
+++ src/Components/Shared/Button/Button.jsx
@@ -1,20 +1,22 @@
-import { Link } from "react-router-dom"
-import PropTypes from 'prop-types';
-const Button = ({text,icon,link,iconFirst}) => {
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+const Button = ({ text, icon, link, iconFirst }) => {
   return (
     <Link to={link} target="_blank">
-    <button className="shadow-in duration-300 w-full ease-in-out rounded-xl text-xs md:text-lg lg:text-2xl font-medium px-2 md:px-4 py-2 hover:shadow-up hover:text-blue-light active:shadow-in active:text-blue-light">
-        <p className="flex justify-center items-center gap-2">{iconFirst} {text} {icon}</p>
-    </button>
+      <button className="shadow-in duration-300 w-full ease-in-out rounded-xl text-xs md:text-lg lg:text-2xl font-medium px-2 md:px-4 py-2 hover:shadow-up hover:text-blue-light active:shadow-in active:text-blue-light">
+        <p className="flex justify-center items-center gap-2">
+          {iconFirst} {text} {icon}
+        </p>
+      </button>
     </Link>
-  )
-}
+  );
+};
 
-Button.propTypes={
+Button.propTypes = {
   text: PropTypes.string,
   icon: PropTypes.element,
   link: PropTypes.string,
-  iconFirst: PropTypes.element
-}
+  iconFirst: PropTypes.element,
+};
 
-export default Button
+export default Button;
