061bc2ff1d2c7b5c5ede434e19b1808949afd0b9
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-11-30 23:08:00 +0600

    reduce animation duration in MyWork component for improved performance

--- src/Pages/MyWork/MyWork.jsx
+++ src/Pages/MyWork/MyWork.jsx
@@ -9,7 +9,7 @@ const MyWork = () => {
       <motion.div
         initial={{ opacity: 0, y: -20 }}
         whileInView={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.5 }}
+        transition={{ duration: 0.3 }}
       >
         <Header text1={"My"} text2={"Projects"} />
       </motion.div>
