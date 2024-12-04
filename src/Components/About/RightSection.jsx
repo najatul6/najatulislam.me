eb4d9cbff614688f81e534015901a8539a7e182a
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-04 20:20:01 +0600

    refactor RightSection component layout; update subtitle, adjust grid structure, and modify displayed statistics

--- src/Components/About/RightSection.jsx
+++ src/Components/About/RightSection.jsx
@@ -9,27 +9,52 @@ const RightSection = () => {
         initial={{ opacity: 0, y: -20 }}
         whileInView={{ opacity: 1, y: 0 }}
         transition={{ duration: 1 }}
-        className="shadow-up flex justify-center items-center py-2 rounded-xl"
+        className="shadow-up w-full flex justify-center items-center py-2 mb-4 rounded-xl"
       >
-        <SubTitle text1={"personal"} text2={"Infos"} />
+        <SubTitle text1={"github"} text2={"Overview"} />
       </motion.div>
-      <div className="w-full h-full flex gap-5">
+      <div className="w-full h-full grid grid-cols-2 gap-5">
+      {/* Total Stars: */}
         <motion.div
           initial={{ opacity: 0 }}
           whileInView={{ opacity: 1 }}
           transition={{ duration: 0.8, delay: 0.2 }}
-          className="shadow-in tex-sm flex flex-col justify-center items-center font-semibold md:text-xl px-2 md:px-5 py-5 rounded-xl w-full"
+          className="shadow-in tex-sm flex flex-col justify-center items-center font-Cormorant font-semibold md:text-xl px-2 md:px-5 py-5 rounded-xl w-full"
         >
           <div className="text-2xl"> 
-            130<span className="text-blue-light">+</span>
+            113<span className="text-blue-light">+</span>
           </div>
-          <span> Longest Streak </span> 
+          <span> Total Stars</span> 
+        </motion.div>
+
+        <motion.div
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          transition={{ duration: 0.8, delay: 0.2 }}
+          className="shadow-in tex-sm flex flex-col justify-center items-center font-Cormorant font-semibold md:text-xl px-2 md:px-5 py-5 rounded-xl w-full"
+        >
+          <div className="text-2xl"> 
+            100<span className="text-blue-light">+</span>
+          </div>
+          <span>Repositories</span> 
         </motion.div>
         <motion.div
           initial={{ opacity: 0 }}
           whileInView={{ opacity: 1 }}
           transition={{ duration: 0.9, delay: 0.3 }}
-          className="shadow-in flex flex-col justify-center items-center tex-sm font-semibold md:text-xl px-2 md:px-5 py-5 rounded-xl w-full"
+          className="shadow-in flex flex-col justify-center items-center tex-sm font-Cormorant font-semibold md:text-xl px-2 md:px-5 py-5 rounded-xl w-full"
+        >
+          <div className="text-2xl">
+            {" "}
+            130<span className="text-blue-light">+</span>
+          </div>{" "}
+          <span> Longest Streak</span>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          transition={{ duration: 0.9, delay: 0.3 }}
+          className="shadow-in flex flex-col justify-center items-center tex-sm font-Cormorant font-semibold md:text-xl px-2 md:px-5 py-5 rounded-xl w-full"
         >
           <div className="text-2xl">
             {" "}
@@ -44,7 +69,7 @@ const RightSection = () => {
           initial={{ opacity: 0, y: 30 }}
           whileInView={{ opacity: 1, y: 0 }}
           transition={{ duration: 1 }}
-          className="flex justify-center items-center w-full col-span-2 mt-10"
+          className="flex justify-center items-center w-full col-span-2 mt-5"
         >
           <button className="download-btn shadow-in rounded-xl">
             <span className="button-bg"></span>
