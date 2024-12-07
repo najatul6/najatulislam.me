a5f11745631092d354c101bfc384b5d47f1c65a5
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-07 16:45:45 +0600

    refactor MyWorkCard component; implement hover effect for image and clean up code structure

--- src/Components/Project/MyWorkCard.jsx
+++ src/Components/Project/MyWorkCard.jsx
@@ -3,54 +3,59 @@ import Button from "../Shared/Button/Button";
 import { IoGitBranch } from "react-icons/io5";
 import { VscLiveShare } from "react-icons/vsc";
 import { motion } from "framer-motion";
+import { useRef } from "react";
 
-const MyWorkCard = ({item}) => {
+const MyWorkCard = ({ item }) => {
+  const imageRef = useRef(null);
+  const handleHover = () => {
+    const height = imageRef.current.clientHeight;
+    imageRef.current.style.transform = `translateY(-${height - 200}px)`;
+  };
+
+  const handleHoverOut = () => {
+    imageRef.current.style.transform = "translateY(0)";
+  };
   return (
     <div className="shadow-in p-2 md:p-3 rounded-xl">
-           <motion.div
-           whileHover={{scale:1}}
-            className="group relative overflow-hidden rounded-xl shadow-in h-[300px]"
-          >
-            <img
-              src={item?.image}
-              alt={item?.name}
-              className=" w-full object-cover transition-transform duration-300 group-hover:scale-110"
+      <motion.div
+        // whileHover={{ scale: 1 }}
+        className="group relative overflow-hidden  shadow-in h-[300px]"
+        onMouseOver={handleHover}
+        onMouseOut={handleHoverOut}
+      >
+        <img
+          src={item?.image}
+          alt={item?.name}
+          ref={imageRef}
+          className=" w-full  transition-transform duration-300 group-hover:scale-110 "
+        />
+        <div className="absolute w-full bottom-0 flex bg-high-black flex-col justify-center items-center px-3 py-5  transition-opacity duration-500  shadow-in">
+          <div className="flex gap-6 justify-start items-center duration-300">
+            <Button
+              text="Git Hub"
+              link={`${item?.github_link}`}
+              icon={<IoGitBranch />}
             />
-            <div className="absolute w-full bottom-0 flex bg-high-black flex-col justify-center items-center px-3 py-5  transition-opacity duration-500  shadow-in">
-              
-              <div className="flex gap-6 justify-start items-center ">
-                <Button
-                  text="Git Hub"
-                  link={`${item?.github_link}`}
-                  icon={<IoGitBranch />}
-                />
-                <Button
-                  text="Live Link"
-                  link={`${item?.liveLink}`}
-                  icon={<VscLiveShare />}
-                />
-              </div>
-            </div>
-          </motion.div>
-         </div>
-  )
-}
+            <Button
+              text="Live Link"
+              link={`${item?.liveLink}`}
+              icon={<VscLiveShare />}
+            />
+          </div>
+        </div>
+      </motion.div>
+    </div>
+  );
+};
 
 MyWorkCard.propTypes = {
   item: PropTypes.object.isRequired,
-}
+};
 
-export default MyWorkCard
+export default MyWorkCard;
 
-
-
-
-
-
-
-
-
-{/* <div className="shadow-in p-2 md:p-3 rounded-xl">
+{
+  /* <div className="shadow-in p-2 md:p-3 rounded-xl">
            <motion.div
            whileHover={{scale:1}}
             className="group relative overflow-hidden rounded-xl shadow-in"
@@ -75,19 +80,20 @@ export default MyWorkCard
                         <li>{listItem}</li>
                     </ul>
                 ))}
-              </div> */}
-        //       <div className="flex gap-6 justify-start items-center ">
-        //         <Button
-        //           text="Git Hub"
-        //           link={`${item?.github_link}`}
-        //           icon={<IoGitBranch />}
-        //         />
-        //         <Button
-        //           text="Live Link"
-        //           link={`${item?.liveLink}`}
-        //           icon={<VscLiveShare />}
-        //         />
-        //       </div>
-        //     </div>
-        //   </motion.div>
-        //  </div> */}
+              </div> */
+}
+//       <div className="flex gap-6 justify-start items-center ">
+//         <Button
+//           text="Git Hub"
+//           link={`${item?.github_link}`}
+//           icon={<IoGitBranch />}
+//         />
+//         <Button
+//           text="Live Link"
+//           link={`${item?.liveLink}`}
+//           icon={<VscLiveShare />}
+//         />
+//       </div>
+//     </div>
+//   </motion.div>
+//  </div> */}
