0b65d8197e9fa1c57edf2bef03372974088b3362
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-12-07 16:48:48 +0600

    refactor MyWorkCard component; improve hover effect logic and enhance transition duration

--- src/Components/Project/MyWorkCard.jsx
+++ src/Components/Project/MyWorkCard.jsx
@@ -9,7 +9,8 @@ const MyWorkCard = ({ item }) => {
   const imageRef = useRef(null);
   const handleHover = () => {
     const height = imageRef.current.clientHeight;
-    imageRef.current.style.transform = `translateY(-${height - 200}px)`;
+    const translateY = height - 200 > 0 ? height - 210 : 0; 
+    imageRef.current.style.transform = `translateY(-${translateY}px)`;
   };
 
   const handleHoverOut = () => {
@@ -27,7 +28,7 @@ const MyWorkCard = ({ item }) => {
           src={item?.image}
           alt={item?.name}
           ref={imageRef}
-          className=" w-full  transition-transform duration-300 group-hover:scale-110 "
+          className=" w-full  transition-transform duration-500 group-hover:scale-110 "
         />
         <div className="absolute w-full bottom-0 flex bg-high-black flex-col justify-center items-center px-3 py-5  transition-opacity duration-500  shadow-in">
           <div className="flex gap-6 justify-start items-center duration-300">
@@ -53,47 +54,3 @@ MyWorkCard.propTypes = {
 };
 
 export default MyWorkCard;
-
-{
-  /* <div className="shadow-in p-2 md:p-3 rounded-xl">
-           <motion.div
-           whileHover={{scale:1}}
-            className="group relative overflow-hidden rounded-xl shadow-in"
-          >
-            <img
-              src={item?.image}
-              alt={item?.name}
-              className="h-[400px] lg:h-[300px] w-full object-cover transition-transform duration-300 group-hover:scale-110"
-            />
-            <div className="absolute inset-0 flex flex-col justify-center items-center px-3 py-5 opacity-0 backdrop-blur-3xl transition-opacity duration-500 group-hover:opacity-100 bg-[#00113b] bg-opacity-80">
-              <h3 className="uppercase mb-2 text-xl font-bold text-white">
-                {item?.name}
-              </h3>
-              <hr  className="w-full py-1"/>
-              <h4 className="mb-2 text-center">{item?.description}</h4>
-              {/* <div className="lg:mb-12">
-                <h3 className="text-lg font-semibold text-white underline">
-                  Features :
-                </h3>
-                {item?.features?.map((listItem, index) => (
-                    <ul key={index} className="pl-5 list-square">
-                        <li>{listItem}</li>
-                    </ul>
-                ))}
-              </div> */
-}
-//       <div className="flex gap-6 justify-start items-center ">
-//         <Button
-//           text="Git Hub"
-//           link={`${item?.github_link}`}
-//           icon={<IoGitBranch />}
-//         />
-//         <Button
-//           text="Live Link"
-//           link={`${item?.liveLink}`}
-//           icon={<VscLiveShare />}
-//         />
-//       </div>
-//     </div>
-//   </motion.div>
-//  </div> */}
