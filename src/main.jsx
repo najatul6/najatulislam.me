46ea35ac972ee40d7f9fb4c1142f00676132af14
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2025-03-03 00:07:21 +0600

    refactor main.jsx for improved code consistency and readability

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
-import { router } from './Router/Router.jsx'
-import './index.css'
-import { Helmet, HelmetProvider } from 'react-helmet-async'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./Router/Router.jsx";
+import "./index.css";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <HelmetProvider>
-    <Helmet>
+      <Helmet>
         <title>Najatul Islam - Official Website</title>
         <meta
           name="description"
           content="Welcome to Najatul Islam's official website. Explore projects, portfolios, and blogs."
         />
-        <meta name="keywords" content="Najatul Islam, portfolio, official website" />
+        <meta
+          name="keywords"
+          content="Najatul Islam, portfolio, official website"
+        />
         <meta name="author" content="Najatul Islam" />
       </Helmet>
-    <RouterProvider router={router} />
+      <RouterProvider router={router} />
     </HelmetProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
