7f76e6d0d6dbeb9fb774ce68acda37e852cab010
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2025-03-02 23:44:46 +0600

    refactor MyWorkCard component to streamline button rendering and improve code readability

--- src/Components/Project/MyWorkCard.jsx
+++ src/Components/Project/MyWorkCard.jsx
@@ -1,5 +1,4 @@
 import PropTypes from "prop-types";
-import Button from "../Shared/Button/Button";
 import { IoGitBranch } from "react-icons/io5";
 import { VscLiveShare } from "react-icons/vsc";
 import { motion } from "framer-motion";
@@ -10,7 +9,7 @@ const MyWorkCard = ({ item }) => {
   const imageRef = useRef(null);
   const handleHover = () => {
     const height = imageRef.current.clientHeight;
-    const translateY = height - 200 > 0 ? height - 210 : 0; 
+    const translateY = height - 200 > 0 ? height - 210 : 0;
     imageRef.current.style.transform = `translateY(-${translateY}px)`;
   };
 
@@ -33,30 +32,28 @@ const MyWorkCard = ({ item }) => {
         />
         <div className="absolute w-full bottom-0 flex bg-high-black flex-col justify-center items-center px-3 py-3  transition-opacity duration-500  shadow-in">
           <div className="flex gap-4 justify-start items-center duration-300 w-full">
-             <Link to={item?.github_link} target="_blank" className="w-full">
-                  <button className="shadow-in w-full  rounded-xl text-base md:text-lg font-medium px-2 md:px-4 py-2 transition-all duration-500 hover:shadow-up hover:text-blue-light active:shadow-in active:text-blue-light">
-                    <p className="flex justify-center items-center gap-2">
-                      Git Hub <span className="animate-bounce text-blue-light"> <IoGitBranch /></span>
-                    </p>
-                  </button>
-                </Link>
-             <Link to={item?.liveLink} target="_blank" className="w-full">
-                  <button className="shadow-in w-full  rounded-xl text-base md:text-lg font-medium px-2 md:px-4 py-2 transition-all duration-500 hover:shadow-up hover:text-blue-light active:shadow-in active:text-blue-light">
-                    <p className="flex justify-center items-center gap-2">
-                    Live Link <span className="animate-bounce text-blue-light"> <VscLiveShare /></span>
-                    </p>
-                  </button>
-                </Link>
-            {/* <Button
-              text="Git Hub"
-              link={`${item?.github_link}`}
-              icon={<IoGitBranch />}
-            /> */}
-            {/* <Button
-              text="Live Link"
-              link={`${item?.liveLink}`}
-              icon={<VscLiveShare />}
-            /> */}
+            <Link to={item?.github_link} target="_blank" className="w-full">
+              <button className="shadow-in w-full  rounded-xl text-base md:text-lg font-medium px-2 md:px-4 py-2 transition-all duration-500 hover:shadow-up hover:text-blue-light active:shadow-in active:text-blue-light">
+                <p className="flex justify-center items-center gap-2">
+                  Git Hub{" "}
+                  <span className="animate-bounce text-blue-light">
+                    {" "}
+                    <IoGitBranch />
+                  </span>
+                </p>
+              </button>
+            </Link>
+            <Link to={item?.liveLink} target="_blank" className="w-full">
+              <button className="shadow-in w-full  rounded-xl text-base md:text-lg font-medium px-2 md:px-4 py-2 transition-all duration-500 hover:shadow-up hover:text-blue-light active:shadow-in active:text-blue-light">
+                <p className="flex justify-center items-center gap-2">
+                  Live Link{" "}
+                  <span className="animate-bounce text-blue-light">
+                    {" "}
+                    <VscLiveShare />
+                  </span>
+                </p>
+              </button>
+            </Link>
           </div>
         </div>
       </motion.div>
