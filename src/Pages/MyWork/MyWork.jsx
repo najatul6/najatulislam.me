1d2839e19aa078d66d5652bbb7cf8987479ae8f8
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2025-03-18 01:24:19 +0600

    enhance MyWork component with Swiper navigation and pagination features

--- src/Pages/MyWork/MyWork.jsx
+++ src/Pages/MyWork/MyWork.jsx
@@ -1,11 +1,12 @@
-
-import { motion } from "framer-motion";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Autoplay } from "swiper/modules";
 import Header from "../../Components/Shared/Header/Header";
 import { projectItem } from "../../Utils/projectItem";
 import MyWorkCard from "../../Components/Project/MyWorkCard";
+import { motion } from "framer-motion";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Navigation, Pagination } from "swiper/modules";
 import "swiper/css";
+import "swiper/css/navigation";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 
 const MyWork = () => {
   return (
@@ -17,19 +18,30 @@ const MyWork = () => {
       >
         <Header text1={"My"} text2={"Projects"} />
       </motion.div>
-      <section className="shadow-in py-4 md:px-5 rounded-xl">
+      <section className="shadow-in py-4 md:px-5 rounded-xl relative">
+        {/* Swiper Navigation Buttons */}
+        <button className="absolute top-1/2 left-0 z-10 bg-gray-800 text-white p-2 rounded-full transform -translate-y-1/2 hover:bg-gray-600 transition" id="prevSlide">
+          <FaArrowLeft />
+        </button>
+        <button className="absolute top-1/2 right-0 z-10 bg-gray-800 text-white p-2 rounded-full transform -translate-y-1/2 hover:bg-gray-600 transition" id="nextSlide">
+          <FaArrowRight />
+        </button>
+
         <Swiper
-          modules={[Autoplay]}
+          modules={[Autoplay, Navigation, Pagination]}
           spaceBetween={20}
           slidesPerView={1}
           loop={true}
+          navigation={{
+            nextEl: "#nextSlide",
+            prevEl: "#prevSlide",
+          }}
           autoplay={{
-            delay: 0,
+            delay: 3000,
             disableOnInteraction: false,
             pauseOnMouseEnter: true,
-            speed: 4500,
           }}
-          speed={4500}
+          speed={1500}
           freeMode={true}
           breakpoints={{
             640: { slidesPerView: 2 },
